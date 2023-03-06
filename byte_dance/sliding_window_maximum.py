ada5d23336c5f939bdaad2bf28858dd4810dff51
lifangjun <lifj@ebondhm.com>
2023-03-06 15:47:20 +0800

    feat:(sliding window maximum)

--- byte_dance/sliding_window_maximum.py
+++ byte_dance/sliding_window_maximum.py
@@ -1,4 +1,5 @@
 import heapq
+from collections import deque
 
 
 class Solution:
@@ -9,7 +10,36 @@ class Solution:
         max_list = [-self.heap[0][0]]
         for i in range(k, len(nums)):
             heapq.heappush(self.heap, (-nums[i], i))
-            while self.heap[0][1] <= i-k:
+            while self.heap[0][1] <= i - k:
                 heapq.heappop(self.heap)
             max_list.append(-self.heap[0][0])
         return max_list
+
+    def max_sliding_window2(self, nums, k):
+        win, res = [], []
+        for i, v in enumerate(nums):
+            if i >= k and win[0] <= i - k:
+                win.pop(0)
+            while win and nums[win[-1]] <= v:
+                win.pop()
+            win.append(i)
+            if i >= k - 1:
+                res.append(nums[win[0]])
+        return res
+
+    def max_sliding_window3(self, nums, k):
+        win, res = deque(), []
+        for i, v in enumerate(nums):
+            if i >= k and win[0] <= i - k:
+                win.popleft()
+            while win and nums[win[-1]] <= v:
+                win.pop()
+            win.append(i)
+            if i >= k - 1:
+                res.append(nums[win[0]])
+        return res
+
+
+if __name__ == '__main__':
+    s = Solution().max_sliding_window3(nums=[1, -1], k=1)
+    print(s)
